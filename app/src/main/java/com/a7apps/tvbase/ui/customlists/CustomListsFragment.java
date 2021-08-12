59ae5f3852548ca7ef7200ed4843d0bdb2705e02
user3941 <user3941.test@gmail.com>
2021-08-12 16:52:10 -0500

    custom lists design

--- app/src/main/java/com/a7apps/tvbase/ui/customlists/CustomListsFragment.java
+++ app/src/main/java/com/a7apps/tvbase/ui/customlists/CustomListsFragment.java
@@ -4,19 +4,23 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import com.a7apps.tvbase.R;
 
 public class CustomListsFragment extends Fragment {
-
-
-
+    private TextView textView;
+    private ImageButton btnAddCustomList;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.fragment_custom_lists, container, false);
+        View view = inflater.inflate(R.layout.fragment_custom_lists, container, false);
+        textView = view.findViewById(R.id.txtEmptyCustomLists);
+        btnAddCustomList = view.findViewById(R.id.btnAddCustomList);
 
 
-        return root;
+
+        return view;
     }
 }
